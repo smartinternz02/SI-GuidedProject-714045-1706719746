06dcc6be1f8f7a6e2a2a52422045ed181603e2e4
Radhika Gour <89262417+radhika-gour@users.noreply.github.com>
2024-03-01 17:29:12 +0530

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,53 @@
+# print("welcome to rooler coaster")
+# height = int(input("what is you height in cm?"))
+# if height>=120:
+#     print("you can ride the roller costerr")
+#     age = int(input("what is your age"))
+#     if age<12:
+#            bill = 5
+#            print("child ticket are $5.")
+#     elif age <=18:
+#           bill = 7
+#           print("youth ticket are $7.")
+#     else:
+#           bill=12
+#           print("adult ticket are $12.")
+
+#     wants_photo = input("do you want a photot taken? y or n")
+#     if wants_photo =="y":
+#          bill+=3
+
+#     print(f" you final bill is ${bill}")
+     
+# else:
+#  print("sporry you to grow")
+
+
+# import random
+# random_side = random.randint(0,1)
+# if random_side ==1:
+#     print("heads")
+# else:
+#     print("tails")
+
+# student_scores = [78,65,89,86,55,91,64,89]
+# highest_score = 0
+# for score in student_scores:
+#     if score > highest_score:
+#         highest_score = score 
+# # print(f"Thee highest score 
+# in the class is :{highest_scores}")
+
+# total = 0
+# for number in range(2,101,2):
+    
+#     print(number )
+# for number in range( 1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#       print("fizzbuzz")
+#     elif number % 3 == 0:
+#       print("fizz")
+#     elif number % 5 == 0:
+#       print("buzz")
+#     else:
+#        print(number)
---
+++ uu.py
@@ -0,0 +1,57 @@
+# print("welcome to the rollercoaster")
+# height = int(input("'what is your height in cm?"))
+
+# if height == 120:
+#  print("you can ride")
+# else:
+#  print("you cannot ride")
+
+# number= int(input("which number do your wantto change"))
+
+# if number % 2==0:
+#  print("this an even number")
+# else:
+#  print("this an odd number")
+
+# number= int(input("what is your age" ))
+# if number >= 100:
+#     print("you can ride")
+#     if age < 25 :
+#         print("you pay $7")
+#     ifelse age<=50:
+#         print("you should pay $18")
+# else:
+#     print("you pay $20")
+
+
+# else:
+#     print("you cannot no ride")
+
+# height = float(input("enter the height in m."))
+# weight = float(input("enter the weight in kg"))
+
+# bmi = round(weight /height ** 2)
+# if bmi <18.5 :
+#     print(f"your bmi is{bmi},your the under weight.")
+# elif bmi < 25 :
+#     print(f"your bmi is{bmi},you have a normal weight")
+# elif bmi <30:
+#     print(f"your bmi is{bmi},you are obese.")
+# elif bmi<35:
+#     print(f"you bmi is {bmi},you are overwight.")
+# else:
+#   print(f"your bmi is{bmi},your are cllinically obese.")   
+
+year = int(input("which year do your want to check?"))
+
+if year % 4 == 0:
+else:
+    print("not leaf year")
+if year % 100 ==0:
+else:
+    print("leap year")
+if year % 400 ==0:
+else:
+    print("not leaf year")
+
+
